75a5c23ea4af462130835ed27c89922ff4fcb8ab
swatiradia <swatisbhovi@gmail.com>
2024-04-05 09:07:45 -0400

    More chages to the framework

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require("cypress");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprocessor,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", preprocessor(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
 
@@ -16,9 +33,7 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    specPattern: 'cypress/integration/example/*.js'
+    setupNodeEvents,
+    specPattern: 'cypress/integration/example/cucumberFiles/*.feature'
   },
 });
